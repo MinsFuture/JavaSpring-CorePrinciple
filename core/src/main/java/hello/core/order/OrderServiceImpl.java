a1850a9ec258c02a8ac16d6fc9c6fdc768d36ab3
MinsFuture <helloaway213@gmail.com>
2023-08-31 20:19:16 +0900

    docs: Designing on the core principles of JavaSpring

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,12 +1,15 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+// @Component
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
