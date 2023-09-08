5d03f238875e58ae2b819a05bb2a6d5bbcf5f578
MinsFuture <helloaway213@gmail.com>
2023-09-08 17:50:50 +0900

    docs: Designing on the core principles of JavaSpring

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자를 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +27,16 @@ public class NetworkClient {
     // 서비스 종료 시 호출
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+    @PostConstruct
+    public void init() { // 의존관계 주입이 끝나면 호출
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        disconnect();
     }
 }
